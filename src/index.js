0c1059dfc5440a3e00d7918b21febce48f797513
Kaeli Gilmore <mskaleemae2000@gmail.com>
2022-08-16 12:00:58 -0400

    practicecode

--- src/index.js
+++ src/index.js
@@ -13,3 +13,43 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const myRequest = new Request ( {
+  method: 'GET',
+  headers: myHeaders, 
+});
+
+
+function addToy(_toy_) {
+  fetch(`http://localhost:3000/toys`),{
+    method:`POST`,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      "name": "Jessie",
+      "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+      "likes": 0
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Success:', data);
+    })
+  }
+  function addBTN(_button_) {
+    fetch(`http://localhost:3000/toys/:id`), {
+    method:`PATCH`,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      "likes": newNumberOfLikes })
+      .then((response) => response.json())
+      .then((likes) => console.log(likes)),
+     querySelector ("#new-toy-btn").addEventListener ('click',() => console.log) 
+  }
+  }
+
+}
